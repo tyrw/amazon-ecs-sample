390b04331976f3505170c09803c0bb3746d33a73
twler <tyler.warnock@gmail.com>
2012-06-11 14:43:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Rails project for testing the amazon-ecs gem
+
+rails new amazon-testing
+gem install amazon-ecs
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmazonTesting::Application.config.secret_token = '46b8b0af7bd7af11a73bb2ea34462e1f2418d6ea5c53d00a4f9161299e85419f5e971b2c6d5bf43145f047d25b9235fba399c01198aacc3ef0470fbed1ab6875'
